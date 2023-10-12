5bd284c74d010d842ee9e1a56d810b079dd16645
ketan <ketanchavan96m@gmail.com>
2023-10-12 15:51:29 +0530

    sad

--- src/app/services/tutorial.service.ts
+++ src/app/services/tutorial.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const baseUrl = 'http://13.127.252.77:8000/api/tutorials';
+const baseUrl = 'http://65.2.3.68:8000/api/tutorials';
 
 @Injectable({
   providedIn: 'root'
