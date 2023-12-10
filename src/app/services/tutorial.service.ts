053a1890e8db9d88d7bf00649c245ef792b69c88
ketankc007 <103125867+ketankc007@users.noreply.github.com>
2023-12-10 07:35:16 +0530

    Update tutorial.service.ts

--- src/app/services/tutorial.service.ts
+++ src/app/services/tutorial.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const baseUrl = 'http://15-188-86-119:8000/api/tutorials';
+const baseUrl = 'http://15.237.149.22:8000/api/tutorials';
 
 @Injectable({
   providedIn: 'root'
